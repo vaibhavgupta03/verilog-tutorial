d9314c4f91a8d0984408a74d95193dc2cf773ea4
Vaibhav Gupta <vaibhavgupta6393@gmail.com>
2026-01-17 22:26:01 +0530

    Enhance 16-bit counter to support up/down counting mode; update testbench and simulation files

--- 16-bit counter/counter16.v
+++ 16-bit counter/counter16.v
@@ -1,15 +1,16 @@
-// Designingb a 16-bit counter in Verilog with synchronous reset and enable signal
+// Designing a 16-bit counter in Verilog with synchronous reset and enable signal and adding a mode for counting up or down
 
-module counter16(clk,rst,enable,count);
+module counter16(clk,rst,enable,count,mode);
 input clk;
 input rst;
 input enable;
+input mode; // mode=0 for up counting, mode=1 for down counting
 output reg[15:0] count=0;
 
 always @(posedge clk) begin
   if(rst)
     count = 16'b0;
   else if(enable)
-    count <= count+1;
+    count <= mode ? count-1 : count+1;
 end
 endmodule
--- 16-bit counter/testcounter.v
+++ 16-bit counter/testcounter.v
@@ -2,6 +2,7 @@ module testcounter;
 reg clk;
 reg rst;
 reg enable;
+reg mode;
 wire[15:0] count;
 
 initial begin
@@ -9,15 +10,16 @@ initial begin
   forever #5 clk = ~clk;
 end
 
-counter16 DUT(clk,rst,enable,count);
+counter16 DUT(clk,rst,enable,count,mode);
 
 initial begin
   $dumpfile("counter16.vcd");
   $dumpvars(0,testcounter);
-  $monitor($time," count is %d and rst is %d enable is %d",count,rst,enable);
-  rst = 1;
+  $monitor($time," count is %d and rst is %d enable is %d mode is %d",count,rst,enable,mode);
+  rst = 1; enable = 0; mode = 0; // start with up counting
   #5 rst = 0; enable = 1;
   #90 enable = 0;
+  #20 mode = 1; // switch to down counting
   #40 enable = 1;
   #40 rst = 1;
   #10 $finish;
